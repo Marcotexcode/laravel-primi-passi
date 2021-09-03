a318bf6731bf0c92db3e066811c9d052b1681a3d
Marco Tessitore <marcotexcode@gmail.com>
2021-09-03 15:03:14 +0200

    init aggiunta rotte

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+    <div class="container">
+                <div class="title m-b-md">
+                    ABOUT
+                </div>
+                <ul>
+                    <li><a href="{{route('homepage')}}">Home</a></li>
+                    <li><a href="{{route('chi-siamo')}}">About</a></li>
+                    <li><a href="{{route('contatti')}}">Contatti</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
